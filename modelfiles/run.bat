b1473a56fe2b92cb229a141493f39aea149406a0
Nick Sanft <nsanft@sanft.com>
2026-01-08 13:37:08 -0600

    Updated modelfiles, run.bat

--- modelfiles/run.bat
+++ modelfiles/run.bat
@@ -1,7 +1,9 @@
 ollama create -f .\gpt-oss-20b-modelfile.txt gpt-oss
 ollama create -f .\llama3.2-modelfile.txt llama3.2
+ollama create -f .\llava-modelfile.txt llava
 
 ollama run gpt-oss /bye
 ollama run llama3.2 /bye
+ollama run llava /bye
 
 ollama pull mxbai-embed-large
