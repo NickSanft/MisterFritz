5f0656f9182be0eb43739c40e327f7474cf5cb9c
Nick Sanft <nsanft@sanft.com>
2026-02-22 16:56:13 -0600

    Added more modelfiles

--- modelfiles/run.bat
+++ modelfiles/run.bat
@@ -1,9 +1,13 @@
+ollama create -f .\gpt-oss-claude-20b-modelfile.txt gpt-oss-claude
 ollama create -f .\gpt-oss-20b-modelfile.txt gpt-oss
 ollama create -f .\llama3.2-modelfile.txt llama3.2
 ollama create -f .\llava-modelfile.txt llava
+ollama create -f .\qwen2.5-coder-modelfile.txt qwen2.5-coder
 
+ollama run gpt-oss-claude /bye
 ollama run gpt-oss /bye
 ollama run llama3.2 /bye
 ollama run llava /bye
+ollama run qwen2.5-coder /bye
 
 ollama pull mxbai-embed-large
